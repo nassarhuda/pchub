a0538ffc3947ad6b554c5a82c5b7bb13c1663c6b
Huda Nassar <hnassar@purdue.edu>
2015-07-18 12:41:50 -0700

    fixed error message

--- error.php
+++ error.php
@@ -24,7 +24,7 @@ header('refresh:4; url=login.php');
     
   </head>
   <body>
-  <div class="alert alert-danger" role="alert">Username not found!</div>
+  <div class="alert alert-danger" role="alert">Login error! Redirecting back to login page</div>
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
